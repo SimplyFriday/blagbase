b854adf3515e2fd99ffc422a55e1fbd56ba74258
Josh Greenlaw <jgreenlaw91@gmail.com>
2020-12-25 19:19:30 -0500

    more wip

--- functions/src/firestoreHelper.ts
+++ functions/src/firestoreHelper.ts
@@ -1,23 +1,31 @@
-import { DocumentSnapshot } from 'firebase-functions/lib/providers/firestore';
+import { DocumentSnapshot, QueryDocumentSnapshot } from 'firebase-functions/lib/providers/firestore';
 import * as admin from 'firebase-admin';
 
 export class FirestoreHelper {
-    async getFirestorePageById(pageId:string): Promise<void | DocumentSnapshot | null> {
-        const firestore_con  = await admin.firestore();
+    async getFirestorePageById(pageId:string): Promise<DocumentSnapshot | null> {
+        let firestore_con  = admin.firestore();
+        let doc = await firestore_con.collection('Content').doc(pageId).get();
+
+        if (!doc.exists) { 
+            console.log('Document' + pageId + ' not found!'); 
+            return null;
+        }
+        else {
+            console.log('Found page: ' + doc.id);
+            return doc;
+        }
+    }
+
+    async getFirestorePosts(limit?:number, sortDesc:boolean = true): Promise<QueryDocumentSnapshot[] | null> {
+        let firestore_con  = admin.firestore();
+        let collection = firestore_con.collection('Content');
         
-        const writeResult = firestore_con.collection('Content').doc(pageId).get().then(doc => {
-            if (!doc.exists) { 
-                console.log('No such document!'); 
-                return null;
-            }
-            else {
-                return doc;
-            }
-        })
-        .catch(err => { 
-            console.log('Error getting document', err);
-        });
-    
-        return writeResult
+        let result = await collection.where('IsPost','==','true').get();
+        
+        if (result.empty) { 
+            console.log('No posts!');
+        }
+            
+        return result.docs;
     }
 }
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,20 +3,32 @@ import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 import * as exphbs from 'express-handlebars';
 import {FirestoreHelper} from './firestoreHelper';
+import { DocumentSnapshot } from 'firebase-functions/lib/providers/firestore';
 
 admin.initializeApp();
-var fsh = new FirestoreHelper();
+let fsh = new FirestoreHelper();
 
-var app = express();
+let app = express();
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+app.locals.copyright = new Date().getFullYear();
 
 app.get('/', async (req, res) => {
-    var db_result = await fsh.getFirestorePageById("home");
-    console.log(req.query);
+    let coreDoc:DocumentSnapshot | null = null;
 
-    if (db_result) {
-        return res.render('index', {db_result});
+    if (req.query.feed) {
+        // TODO go to feed page
+    } else if (!req.query.content)
+    {
+        coreDoc = await fsh.getFirestorePageById("HomePage");
+        // TODO handle not exists
+    } else {
+        coreDoc = await fsh.getFirestorePageById(req.query.content as string);
+    }
+
+    if (coreDoc) {
+        let data = coreDoc.data();
+        return res.render('render_page', {data});
     } else {
         console.log ("Rendering error page!");
         return res.render('error');
